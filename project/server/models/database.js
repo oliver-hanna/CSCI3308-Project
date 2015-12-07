53b0bcd486a5986bc2144a9fcd4d69ea478a3811
Para4 Company Laptop <para4companylaptop@Para4s-MacBook-Pro.local>
2015-12-07 13:47:40 -0700

    finished commenting code

--- project/server/models/database.js
+++ project/server/models/database.js
@@ -1,3 +1,4 @@
+//Set up connection to DB (currently local host because heroku is being a pain)
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/3308project';
 
